e7265c17802a5bb6e7eb295c612cce080f1e71fc
47Mags47 <gord_ka47@mail.ru>
2024-08-20 16:13:45 +0700

    load

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv  } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+const env = loadEnv('all', process.cwd());
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.sass',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: true,
+        port: env.VITE_ASSET_PORT,
+        strictPort: true,
+        hmr: {
+            host: env.VITE_ASSET_HOST,
+            port: env.VITE_ASSET_PORT,
+        },
+    },
+});
